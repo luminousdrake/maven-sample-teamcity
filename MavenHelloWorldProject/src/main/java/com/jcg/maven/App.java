cc76b46b33561d70d53c7c4fb3dc941210609f55
Dmytro Bukanov <dmytro.bukanov@gmail.com>
2020-01-21 13:54:06 +0100

    Initial commit

---
+++ MavenHelloWorldProject/src/main/java/com/jcg/maven/App.java
@@ -0,0 +1,18 @@
+package com.jcg.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    System.out.println( "Hello World!" );
+    int a = 10;
+    int b = 24;
+    int sum = a + b;
+    System.out.println(sum);
+    }
+
+}
